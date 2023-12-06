4caaa15b331f44cf728e97b65afff9b09c47c8d9
cfdoud <prouddoud01@gmail.com>
2023-12-06 14:02:14 -0800

    Register page

--- Presentation/Phase3/api.py
+++ Presentation/Phase3/api.py
@@ -1,6 +1,7 @@
 #use flask to create a web app for super smash bros ultimate tournament creation
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
+from sqlite3 import Error
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,7 +15,205 @@ def character_page():
 @app.route('/register')
 def register_page():
     return render_template('register.html')
+def register_page():
+    if request.method == 'POST, GET':
+        name = request.form['name']
+        sponsor = request.form['sponsor']
+        country = request.form['country']
+        controller = request.form['controller']
+        main = request.form['main']
+        secondary = request.form['secondary']
+        tertiary = request.form['tertiary']
+        conn = openConnection("ssb.db")
+        cur = conn.cursor()
+        cur.execute("INSERT INTO player (player_name, sponsor, countryID, controllertype) VALUES (?, ?, ?, ?)", (name, sponsor, country, controller))
+        cur.execute("INSERT INTO playerCharacter (playerID, charID) VALUES (?, ?)", (name, main))
+        cur.execute("INSERT INTO playerCharacter (playerID, charID) VALUES (?, ?)", (name, secondary))
+        cur.execute("INSERT INTO playerCharacter (playerID, charID) VALUES (?, ?)", (name, tertiary))
+        conn.commit()
+        conn.close()
+        flash("You have been registered!")
+        return redirect(url_for('home'))
+    else:
+        return render_template('register.html')
+    
+
+
+def openConnection(_dbFile):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    print("++++++++++++++++++++++++++++++++++")
+    print("Open database: ", _dbFile)
+
+    conn = None
+    try:
+        conn = sqlite3.connect(_dbFile)
+        print("success")
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+    return conn
+
+def closeConnection(_conn, _dbFile):
+    """ close the connection to the database
+        specified by the db_file"""
+    print("++++++++++++++++++++++++++++++++++")
+    print("Close database: ", _dbFile)
+
+    try:
+        _conn.close()
+        print("success")
+    except Error as e:
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+def createTables(_conn):
+    """ create all the tables for the database with
+        their columns and corresponding datatypes"""
+    print("++++++++++++++++++++++++++++++++++")
+    print("Create tables")
+
+    _conn.execute("BEGIN")
+    try:
+        sql = """CREATE TABLE character (
+                    charID integer,
+                    c_name text,
+                    origin text,
+                    description text)"""
+        _conn.execute(sql)
+
+        sql = """CREATE TABLE country (
+                    countryID integer,
+                    country_name text)"""
+        _conn.execute(sql)
+
+        sql = """CREATE TABLE player (
+                playerID INTEGER,
+                player_name text,
+                rank INTEGER,
+                sponsor text,
+                controllertype text,
+                countryID integer)"""
+        _conn.execute(sql)
+
+        sql = """CREATE TABLE playerCharacter (
+                playerID integer,
+                charID integer)"""
+        _conn.execute(sql)
+
+        sql = """CREATE TABLE playerMatch (
+                playerID integer,
+                matchID integer)"""
+        _conn.execute(sql)
+
+        sql = """CREATE TABLE playerTournament (
+                playerID integer,
+                eventID integer,
+                winnings decimal,
+                placement integer)"""
+        _conn.execute(sql)
+
+        sql = """CREATE TABLE round (
+                roundID integer,
+                matchID integer,
+                result text)"""
+        _conn.execute(sql)
+
+        sql = """CREATE TABLE roundStage (
+                stageID integer,
+                roundID integer)"""
+        _conn.execute(sql)
+
+        sql = """CREATE TABLE shoubu (
+                matchID integer,
+                eventID integer,
+                result text)"""
+        _conn.execute(sql)
+
+        sql = """CREATE TABLE stage (
+                stageID integer,
+                name text,
+                origin text)"""
+        _conn.execute(sql)
+
+        sql = """CREATE TABLE tournament (
+                eventID integer,
+                tournament_name text,
+                year integer)"""
+        _conn.execute(sql)
+
+        _conn.execute("COMMIT")
+        print("success")
+    except Error as e:
+        _conn.execute("ROLLBACK")
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+def dropTable(_conn, _table):
+    """ drop a given table
+    :param _table: table name"""
+    print("++++++++++++++++++++++++++++++++++")
+    print("Drop ", _table)
+
+    try:
+        sql = "DROP TABLE {}".format(_table)
+        #args = [_table]
+        _conn.execute(sql)
+
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+def insertChar(_conn, _cID, _name, _origin, _description):
+    # insert given data into the character table
+    print("++++++++++++++++++++++++++++++++++")
+    print("Insert character")
+
+    try:
+        sql = """INSERT INTO character(charID, c_name, origin, description)
+            VALUES(?, ?, ?, ?)"""
+        args = [_cID, _name, _origin, _description]
+        _conn.execute(sql, args)
+
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
+
+def insertCountry(_conn, _cID, _name):
+    # insert given data into the country table
+    print("++++++++++++++++++++++++++++++++++")
+    print("Insert country")
+
+    try:
+        sql = """INSERT INTO country(countryID, country_name)
+            VALUES(?, ?)"""
+        args = [_cID, _name]
+        _conn.execute(sql, args)
+
+        _conn.commit()
+        print("success")
+    except Error as e:
+        _conn.rollback()
+        print(e)
+
+    print("++++++++++++++++++++++++++++++++++")
 
 
 if __name__ == '__main__':
+    
     app.run(debug=True)
