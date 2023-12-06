4407784006930f8c92b3528bbd82d9d93975fe4f
Christian <prouddoud01@gmail.com>
2023-12-06 15:44:39 -0800

    Roster

--- Presentation/Phase3/api.py
+++ Presentation/Phase3/api.py
@@ -1,12 +1,24 @@
 #use flask to create a web app for super smash bros ultimate tournament creation
+
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
 from sqlite3 import Error
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/roster')
+def roster_page():
+    conn = openConnection("./ssb.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM player")
+    players = cur.fetchall()
+    conn.close()
+    return render_template('roster.html', players=players)
+
 
 @app.route('/character')
 def character_page():
@@ -17,6 +29,7 @@ def register_page():
     return render_template('register.html')
 def register_page():
     if request.method == 'POST, GET':
+
         name = request.form['name']
         sponsor = request.form['sponsor']
         country = request.form['country']
@@ -24,13 +37,13 @@ def register_page():
         main = request.form['main']
         secondary = request.form['secondary']
         tertiary = request.form['tertiary']
-        conn = openConnection("ssb.db")
+        conn = openConnection("./ssb.db")
         cur = conn.cursor()
         cur.execute("INSERT INTO player (player_name, sponsor, countryID, controllertype) VALUES (?, ?, ?, ?)", (name, sponsor, country, controller))
         cur.execute("INSERT INTO playerCharacter (playerID, charID) VALUES (?, ?)", (name, main))
         cur.execute("INSERT INTO playerCharacter (playerID, charID) VALUES (?, ?)", (name, secondary))
         cur.execute("INSERT INTO playerCharacter (playerID, charID) VALUES (?, ?)", (name, tertiary))
-        conn.commit()
+        conn.commit()   
         conn.close()
         flash("You have been registered!")
         return redirect(url_for('home'))
